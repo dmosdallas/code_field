b0aa212d991b9c038669e5ac9d6cafdec08380f9
Dallas McCall <dallas@dmos.tools>
2024-04-18 16:32:02 -0400

    Revert "Merge branch 'BertrandBev:master' into contextMenuBuilder"
    This reverts commit 0349e7445e9e934cbc8e2f34227fe6c57153cc7a, reversing
    changes made to 1336d3496d7f11d4695e98583292c4d2295da5d0.

--- lib/src/code_field/code_field.dart
+++ lib/src/code_field/code_field.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-import 'dart:io';
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 
@@ -232,21 +229,17 @@ class _CodeFieldState extends State<CodeField> {
       ),
     );
 
-    return MediaQuery(
-      // TODO: Temporary fix: https://github.com/flutter/flutter/issues/127017
-      data: !kIsWeb && Platform.isIOS
-          ? const MediaQueryData(
-              gestureSettings: DeviceGestureSettings(touchSlop: 8),
-            )
-          : MediaQuery.of(context),
-      child: SingleChildScrollView(
-        padding: EdgeInsets.only(
-          left: leftPad,
-          right: widget.padding.right,
-        ),
-        scrollDirection: Axis.horizontal,
-        child: intrinsic,
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(
+        left: leftPad,
+        right: widget.padding.right,
       ),
+      scrollDirection: Axis.horizontal,
+
+      /// Prevents the horizontal scroll if horizontalScroll is false
+      physics:
+          widget.horizontalScroll ? null : const NeverScrollableScrollPhysics(),
+      child: intrinsic,
     );
   }
 
